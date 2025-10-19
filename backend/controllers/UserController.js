fb5c26bf74a34daeafa5acc46fb109b592a08e0f
Mahdiyar <mahdiyarsne98@gmail.com>
2025-10-19 13:18:28 +0330

    async

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,7 +3,6 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import path from 'path';
 import fs from 'fs';
-import { url } from 'inspector';
 
 export const getAllUsers = async (req, res) => {
   try {
