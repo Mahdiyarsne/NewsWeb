1bdd22e9a6a777c67eb2e17142924e47691fec3a
Mahdiyar <mahdiyarsne98@gmail.com>
2025-10-22 13:19:12 +0330

    بخش اپلود ویدیو اتمام شد.

--- backend/controllers/VideoController.js
+++ backend/controllers/VideoController.js
@@ -1,6 +1,18 @@
 import Video from '../models/videoModel.js';
 import path from 'path';
+import fs from 'fs';
 
+//دریافت تمامی ویدیو ها
+export const getAllVideo = async (req, res) => {
+  try {
+    const video = await Video.findAll({});
+    res.json(video);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//ساخت ویدیو
 export const createVideo = async (req, res) => {
   if (req.files == null) return res.json({ msg: 'ویدیو انتخاب نشد' });
   const file = req.files.file;
@@ -8,10 +20,12 @@ export const createVideo = async (req, res) => {
   const ext = path.extname(file.name);
   let dateNow = Math.round(Date.now());
   const fileName = dateNow + ext;
-  const url = `${req.protocol}://${req.get('host')}/${fileName}`;
+  const url = `${req.protocol}://${req.get('host')}/video/${fileName}`;
   const allowedType = ['.mp4'];
   if (!allowedType.includes(ext.toLowerCase())) {
-    return res.json({ msg: ' .mp4 * فرمت ویدیو معتبر نیست و از مجاز ' });
+    return res
+      .status(403)
+      .json({ msg: ' .mp4 * فرمت ویدیو معتبر نیست و از مجاز ' });
   }
   if (fileSize > 5000000)
     return res.json({ msg: '.حجم ویدیو نباید 5 مگابایت بیشتر باشد' });
@@ -26,4 +40,29 @@ export const createVideo = async (req, res) => {
       console.log(err.message);
     }
   });
+};
+
+//دریافت یک ویدیو
+export const getSingleVideo = async (req, res) => {
+  try {
+    const video = await Video.findOne({ order: [['createdAt', 'DESC']] });
+    res.json(video);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//حذف ویدیو از طریق ایدی
+export const deleteVideo = async (req, res) => {
+  const video = await Video.findOne({ where: { id: req.params.id } });
+  if (!video) return res.status(404).json({ msg: 'ویدیو یافت نشد' });
+
+  try {
+    const filePath = `./public/video/${video.video}`;
+    fs.unlinkSync(filePath);
+    await video.destroy({ where: { id: req.params.id } });
+    res.status(201).json({ msg: 'ویدیو با موفقعیت حذف شد  ' });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- backend/routes/videoRoutes.js
+++ backend/routes/videoRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { createVideo } from '../controllers/VideoController.js';
+import {
+  createVideo,
+  deleteVideo,
+  getAllVideo,
+  getSingleVideo,
+} from '../controllers/VideoController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
+router.get('/api/get-video', verifyToken, getAllVideo);
+router.get('/api/single-video', getSingleVideo);
 router.post('/api/create-video', verifyToken, createVideo);
+router.delete('/api/delete-video/:id', verifyToken, deleteVideo);
 
 export default router;
