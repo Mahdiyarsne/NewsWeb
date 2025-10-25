a8b62ae41c18da5a502667052a357d3c8bc6f4c1
Mahdiyar <mahdiyarsne98@gmail.com>
2025-10-25 12:40:57 +0330

    بخش ویرایش خبر اضافه شد

--- backend/controllers/NewsController.js
+++ backend/controllers/NewsController.js
@@ -1,5 +1,6 @@
 import News from '../models/newsModel.js';
 import path from 'path';
+import fs from 'fs';
 
 export const getAllNews = async (req, res) => {
   try {
@@ -44,4 +45,64 @@ export const createNews = async (req, res) => {
       console.log(error);
     }
   });
+};
+
+export const getNewsById = async (req, res) => {
+  try {
+    const response = await News.findOne({ where: { id: req.params.id } });
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateNews = async (req, res) => {
+  const news = await News.findOne({ where: { id: req.params.id } });
+  if (!news) return res.status(404).json({ msg: 'دیتایی یافت نشد' });
+
+  let fileName = '';
+  if (req.files == null) {
+    fileName = news.image;
+  } else {
+    const file = req.files.file;
+    const fileSize = file.data.length;
+    const ext = path.extname(file.name);
+    let dateNow = Math.round(Date.now());
+    fileName = dateNow + ext;
+    const allowedType = ['.png', '.jpg', '.jpeg'];
+    if (!allowedType.includes(ext.toLowerCase())) {
+      return res
+        .status(403)
+        .json({ msg: ' .png,.jpg,.jpeg * فرمت عکس معتبر نیست و از مجاز ' });
+    }
+    if (fileSize > 5000000)
+      return res.json({ msg: '.حجم عکس نباید 5 مگابایت بیشتر باشد' });
+
+    const filePath = `./public/avatar/${news.image}`;
+    fs.unlinkSync(filePath);
+    file.mv(`./public/avatar/${fileName}`, (err) => {
+      if (err) return res.json({ msg: err.message });
+    });
+  }
+
+  const { title, desc, userId, catId } = req.body;
+  const url = `${req.protocol}://${req.get('host')}/avater/${fileName}`;
+
+  try {
+    await News.update(
+      {
+        title: title,
+        desc: desc,
+        userId: userId,
+        catId: catId,
+        image: fileName,
+        url: url,
+      },
+      { where: { id: req.params.id } }
+    );
+
+    res.json({ msg: 'خبر با موفقعیت ویرایش شد' });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- backend/routes/newsRoute.js
+++ backend/routes/newsRoute.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { createNews, getAllNews } from '../controllers/NewsController.js';
+import {
+  createNews,
+  getAllNews,
+  getNewsById,
+  updateNews,
+} from '../controllers/NewsController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/api/get-news', verifyToken, getAllNews);
+router.get('/api/get-news/:id', verifyToken, getNewsById);
 router.post('/api/create-news', verifyToken, createNews);
-
+router.put('/api/update-news/:id', verifyToken, updateNews);
 export default router;
