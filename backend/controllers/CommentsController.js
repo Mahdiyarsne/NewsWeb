3b15f458451fd08644bae497af6bfb05f57de2e4
Mahdiyar <mahdiyarsne98@gmail.com>
2025-10-27 14:32:58 +0330

    بخش ویرایش و حذف نظر اضافه شد

--- backend/controllers/CommentsController.js
+++ backend/controllers/CommentsController.js
@@ -1,10 +1,60 @@
 import Comments from '../models/commentModel.js';
 
+//دریافت تمامی نظرات
 export const getAllComments = async (req, res) => {
   try {
     const comments = await Comments.findAll({});
     res.json(comments);
-  } catch (error) { 
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//ساخت نظر
+export const createComment = async (req, res) => {
+  const { newsId, name, description, email, subject } = req.body;
+  try {
+    await Comments.create({
+      newsId,
+      name,
+      description,
+      email,
+      subject,
+    });
+    res.json({ msg: ' نظر شما ارسال شد و پس از تایید آدمین نمایش داده میشود' });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//ویرایش نظر
+export const updateComment = async (req, res) => {
+  const { name, description, subject } = req.body;
+  try {
+    await Comments.update(
+      {
+        name,
+        description,
+        subject,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    res.json({ msg: 'نظر با موفقعیت ویرایش شد' });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//حذف نظر
+export const deleteComment = async (req, res) => {
+  try {
+    await Comments.destroy({
+      where: { id: req.params.id },
+    });
+    res.json({ msg: 'نظر با موفقعیت حدف شد' });
+  } catch (error) {
     console.log(error);
   }
 };
--- backend/routes/commentsRoute.js
+++ backend/routes/commentsRoute.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { getAllComments } from '../controllers/CommentsController.js';
+import {
+  createComment,
+  deleteComment,
+  getAllComments,
+  updateComment,
+} from '../controllers/CommentsController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 const router = express.Router();
 
-router.get('/api/get-comments', verifyToken, getAllComments);
-
+router.get('/api/get-comment', verifyToken, getAllComments);
+router.post('/api/create-comment', createComment);
+router.put('/api/update-comment/:id', verifyToken, updateComment);
+router.delete('/api/delete-comment/:id', verifyToken, deleteComment);
 export default router;
