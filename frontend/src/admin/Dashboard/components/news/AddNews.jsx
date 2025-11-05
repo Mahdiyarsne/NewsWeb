767a7ab9e1dc005264b9775bb574b26e3d394621
Mahdiyar <mahdiyarsne98@gmail.com>
2025-11-05 21:52:17 +0330

    فرم ارسال طراحی شد

--- frontend/src/admin/Dashboard/components/news/AddNews.jsx
+++ frontend/src/admin/Dashboard/components/news/AddNews.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import Dashboard from '../../Dashboard';
+import './news.css';
 
 const AddNews = () => {
   return (
-    <div>AddNews</div>
-  )
-}
+    <Dashboard>
+      <form>
+        <div className='field'>
+          <label className='label'>عنوان خبر</label>
+          <div className='control'>
+            <input
+              type='text'
+              className='input has-text-black has-background-white '
+            />
+          </div>
+        </div>
+        <div className='field'>
+          <label className='label'>متن خبر</label>
+          <div className='control'>
+            <textarea className='textarea'></textarea>
+          </div>
+        </div>
+
+        <div className='field'>
+          <label className='label'>دسته بندی خبر</label>
+          <div className='control'>
+            <div className='select is-fullwidth  '>
+              <select className='has-text-black has-background-white'>
+                <option>تست</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className='field'>
+          <label className='label'>عکس خبر</label>
+          <div className='control'>
+            <input
+              type='file'
+              className='input has-text-black has-background-white '
+            />
+          </div>
+        </div>
+        <div className='field'>
+          <div className='control'>
+            <button className='button is-success px-6 has-text-white'>
+              دخیره
+            </button>
+          </div>
+        </div>
+      </form>
+    </Dashboard>
+  );
+};
 
 export default AddNews;
